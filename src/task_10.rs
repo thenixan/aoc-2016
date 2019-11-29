14277fa6994bf34f9c425d1daadc61dda33f73b4
Ilya Nixan <thenixan@gmail.com>
2019-11-29 23:19:20 +0300

    Day 10 wip

---
+++ src/task_10.rs
@@ -0,0 +1,209 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::hash::{Hash, Hasher};
+use std::io::{BufRead, BufReader};
+use std::iter::{FromIterator, IntoIterator};
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+struct Package {
+    value: usize,
+    target: Target,
+}
+
+impl Package {
+    fn new(value: usize, target: Target) -> Self {
+        Package { value, target }
+    }
+}
+
+impl FromStr for Package {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(" ").collect::<Vec<&str>>();
+        let value = parts[1].parse::<usize>().unwrap();
+        let to = parts[5].parse::<usize>().unwrap();
+        let to = match parts[4] {
+            "bot" => Target::to_bot(to),
+            "output" => Target::to_output(to),
+            _ => return Err(()),
+        };
+        Ok(Package::new(value, to))
+    }
+}
+
+#[derive(Debug)]
+struct Output {
+    number: usize,
+}
+
+#[derive(Debug, Clone)]
+enum Target {
+    ToBot(usize),
+    ToOutput(usize),
+}
+
+impl Target {
+    fn to_bot(number: usize) -> Self {
+        Target::ToBot(number)
+    }
+    fn to_output(number: usize) -> Self {
+        Target::ToOutput(number)
+    }
+}
+
+#[derive(Debug)]
+struct Bot {
+    number: usize,
+    lower_to: Target,
+    higher_to: Target,
+}
+
+impl Bot {
+    fn new(number: usize, lower_to: Target, higher_to: Target) -> Self {
+        Bot {
+            number,
+            lower_to,
+            higher_to,
+        }
+    }
+}
+
+impl FromStr for Bot {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(" ").collect::<Vec<&str>>();
+        let number = parts[1].parse::<usize>().unwrap();
+        let l_to = parts[6].parse::<usize>().unwrap();
+        let h_to = parts[11].parse::<usize>().unwrap();
+        let l_to = match parts[5] {
+            "bot" => Target::to_bot(l_to),
+            "output" => Target::to_output(l_to),
+            _ => return Err(()),
+        };
+        let h_to = match parts[10] {
+            "bot" => Target::to_bot(h_to),
+            "output" => Target::to_output(h_to),
+            _ => return Err(()),
+        };
+        Ok(Bot::new(number, l_to, h_to))
+    }
+}
+
+impl PartialEq for Bot {
+    fn eq(&self, other: &Self) -> bool {
+        self.number == other.number
+    }
+}
+impl Eq for Bot {}
+
+impl Hash for Bot {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.number.hash(state);
+    }
+}
+
+#[derive(Debug)]
+struct Factory {
+    inputs: Vec<Package>,
+    outputs: Vec<Output>,
+    bots: Vec<Bot>,
+}
+
+impl Factory {
+    fn work(&self) -> FactoryIterator {
+        FactoryIterator::new(self)
+    }
+}
+
+impl FromIterator<String> for Factory {
+    fn from_iter<I: IntoIterator<Item = String>>(iter: I) -> Self {
+        let mut inputs = vec![];
+        let mut bots = vec![];
+        let mut outputs = vec![];
+
+        for l in iter {
+            if l.starts_with("bot ") {
+                bots.push(l.parse::<Bot>().unwrap());
+            } else if l.starts_with("value ") {
+                inputs.push(l.parse::<Package>().unwrap());
+            }
+        }
+
+        Factory {
+            inputs,
+            outputs,
+            bots,
+        }
+    }
+}
+
+struct FactoryIterator<'a> {
+    inputs: std::vec::IntoIter<Package>,
+    factory: &'a Factory,
+    state: HashMap<usize, Vec<usize>>,
+}
+
+impl<'a> FactoryIterator<'a> {
+    fn new(factory: &'a Factory) -> Self {
+        FactoryIterator {
+            inputs: factory.inputs.clone().into_iter(),
+            factory,
+            state: HashMap::new(),
+        }
+    }
+}
+impl Iterator for FactoryIterator<'_> {
+    type Item = FactoryHistoryRecord;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.inputs.next() {
+            None => {
+                let bot = self
+                    .state
+                    .iter()
+                    .find_map(|(k, v)| if v.len() == 2 { Some(k) } else { None })
+                    .take();
+                if bot.is_some() {
+                    let bot = bot.unwrap();
+                    let values = self.state.get_mut(bot).take();
+                    println!("Bot: {:?}", bot);
+                    None
+                } else {
+                    None
+                }
+            }
+            Some(p) => {
+                println!("Input: {:?}", p);
+                match p.target {
+                    Target::ToBot(b) => self.state.entry(b).or_insert(vec![]).push(p.value),
+                    Target::ToOutput(_) => (),
+                };
+                Some(FactoryHistoryRecord::Input { package: p })
+            }
+        }
+    }
+}
+
+enum FactoryHistoryRecord {
+    Comparation { bot: usize, values: (usize, usize) },
+    Transmission { from_bot: usize, package: Package },
+    Input { package: Package },
+}
+
+pub fn run() {
+    let input = File::open("input/task_10").unwrap();
+    let input = BufReader::new(input);
+
+    let factory = input.lines().filter_map(|l| l.ok()).collect::<Factory>();
+
+    let result = factory.work().find_map(|r| match r {
+        FactoryHistoryRecord::Comparation { bot, values } => Some("bot"),
+        _ => None,
+    });
+
+    println!("{:?}", result);
+}
+
+pub fn run_e() {
+    let _input = File::open("input/task_9").unwrap();
+}
