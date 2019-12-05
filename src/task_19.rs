7272c6c85e17e4ca0ec75a3090edde12ed70d86f
Ilya Linnik <thenixan@gmail.com>
2019-12-05 17:02:17 +0300

    Day 19

--- src/task_19.rs
+++ src/task_19.rs
@@ -1,39 +1,87 @@
-use std::collections::{LinkedList, VecDeque};
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::Read;
+use std::ops::Index;
 use std::str::FromStr;
 
-struct Game(VecDeque<usize>);
+struct Game {
+    first_half: VecDeque<usize>,
+    second_half: VecDeque<usize>,
+}
+
+impl Index<usize> for Game {
+    type Output = usize;
+    fn index(&self, i: usize) -> &Self::Output {
+        if i < self.first_half.len() {
+            &self.first_half[i]
+        } else {
+            &self.second_half[i - self.first_half.len()]
+        }
+    }
+}
 
 impl FromStr for Game {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let count = s.parse::<usize>().unwrap();
-        Ok(Game((1..=count).collect()))
+        let mid_point = count / 2;
+        Ok(Game {
+            first_half: (1..=mid_point).collect(),
+            second_half: (mid_point + 1..=count).collect(),
+        })
     }
 }
 
 impl Game {
-    fn play(&mut self) -> usize {
-        while self.0.len() > 1 {
-            let first = self.0.pop_front().unwrap();
-            self.0.pop_front();
-            self.0.push_back(first);
+    fn pop_front(&mut self) -> Option<usize> {
+        if self.first_half.is_empty() {
+            self.second_half.pop_front()
+        } else {
+            self.first_half.pop_front()
         }
-        self.0[0]
+    }
+
+    fn push_back(&mut self, value: usize) {
+        self.second_half.push_back(value);
+    }
+
+    fn len(&self) -> usize {
+        self.first_half.len() + self.second_half.len()
+    }
+
+    fn stabilize(&mut self) {
+        while self.first_half.len() < self.second_half.len() {
+            self.first_half
+                .push_back(self.second_half.pop_front().unwrap())
+        }
+    }
+
+    fn play(&mut self) -> usize {
+        while self.len() > 1 {
+            let first = self.pop_front().unwrap();
+            self.pop_front();
+            self.push_back(first);
+        }
+        self[0]
+    }
+
+    fn remove(&mut self, i: usize) -> Option<usize> {
+        if i < self.first_half.len() {
+            self.first_half.remove(i)
+        } else {
+            self.second_half.remove(i - self.first_half.len())
+        }
     }
 
     fn play_b(&mut self) -> usize {
-        while self.0.len() > 1 {
-            if self.0.len() % 1000 == 0 {
-                println!("L: {}", self.0.len());
-            }
-            let length = self.0.len() / 2;
-            let first = self.0.pop_front().unwrap();
-            self.0.remove(length);
-            self.0.push_back(first);
+        while self.len() > 1 {
+            let length = self.len() / 2;
+            self.remove(length);
+            let first = self.pop_front().unwrap();
+            self.push_back(first);
+            self.stabilize()
         }
-        self.0[0]
+        self[0]
     }
 }
 
