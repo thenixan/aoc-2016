ae4d2b140e764ff6c0a3c96b72a583f6a819ac19
Ilya Nixan <thenixan@gmail.com>
2019-12-08 23:07:28 +0300

    Day 21 wip

--- src/task_21.rs
+++ src/task_21.rs
@@ -1,7 +1,12 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
-enum Commands {
+struct CommandSequence {
+    data: Vec<Command>,
+}
+
+enum Command {
     SwapPosition { left: usize, right: usize },
     SwapLetter { left: char, right: char },
     RotateLeft(usize),
@@ -9,6 +14,35 @@ enum Commands {
     RotateWithPosition(char),
     ReversePositions { from: usize, to: usize },
     MoveToPosition { from: usize, to: usize },
+}
+
+impl FromStr for Command {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.split(" ").collect::<Vec<&str>>();
+        match (s[0], s[1]) {
+            ("swap", "position") => Ok(Command::SwapPosition {
+                left: s[2].parse::<usize>().unwrap(),
+                right: s[5].parse::<usize>().unwrap(),
+            }),
+            ("swap", "letter") => Ok(Command::SwapLetter {
+                left: s[2].parse::<char>().unwrap(),
+                right: s[5].parse::<char>().unwrap(),
+            }),
+            ("rotate", "left") => Ok(Command::RotateLeft(s[2].parse::<usize>().unwrap())),
+            ("rotate", "right") => Ok(Command::RotateRight(s[2].parse::<usize>().unwrap())),
+            ("rotate", "based") => Ok(Command::RotateWithPosition(s[6].parse::<char>().unwrap())),
+            ("reverse", "positions") => Ok(Command::ReversePositions {
+                from: s[2].parse::<usize>().unwrap(),
+                to: s[4].parse::<usize>().unwrap(),
+            }),
+            ("move", "position") => Ok(Command::MoveToPosition {
+                from: s[2].parse::<usize>().unwrap(),
+                to: s[5].parse::<usize>().unwrap(),
+            }),
+            _ => Err(()),
+        }
+    }
 }
 
 pub fn run() {
