22c34c7c851927c473099843e0a9a467c4bda031
Ilya Linnik <thenixan@gmail.com>
2019-12-09 13:33:06 +0300

    Reworked day 22

--- src/task_22.rs
+++ src/task_22.rs
@@ -1,39 +1,75 @@
-use regex::Regex;
+use objects::Nodes;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::iter::FromIterator;
-use std::str::FromStr;
 
-struct Node {
-    x: usize,
-    y: usize,
-    used: usize,
-    available: usize,
-}
+mod objects {
+    use regex::Regex;
+    use std::collections::HashMap;
+    use std::iter::FromIterator;
 
-impl Node {
-    fn total(&self) -> usize {
-        self.used + self.available
+    #[derive(Hash, Eq, PartialEq)]
+    pub struct NodePlacement {
+        x: usize,
+        y: usize,
     }
-}
 
-impl FromStr for Node {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let pattern =
-            Regex::new(r"^/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+\d+%$")
-                .unwrap();
-        let caps = pattern.captures(s).unwrap();
-        let x = caps[1].parse::<usize>().unwrap();
-        let y = caps[2].parse::<usize>().unwrap();
-        let u = caps[4].parse::<usize>().unwrap();
-        let a = caps[5].parse::<usize>().unwrap();
-        Ok(Node {
-            x,
-            y,
-            used: u,
-            available: a,
-        })
+    impl NodePlacement {
+        pub fn new(x: usize, y: usize) -> Self {
+            NodePlacement { x, y }
+        }
+    }
+
+    pub struct NodeMeta {
+        used: usize,
+        available: usize,
+    }
+
+    impl NodeMeta {
+        pub fn used(&self) -> usize {
+            self.used
+        }
+        pub fn available(&self) -> usize {
+            self.available
+        }
+    }
+    pub struct Nodes(HashMap<NodePlacement, NodeMeta>);
+
+    impl Nodes {
+        pub fn width(&self) -> usize {
+            self.0.iter().map(|(k, _)| k).map(|k| k.x).max().unwrap() + 1
+        }
+        pub fn height(&self) -> usize {
+            self.0.iter().map(|(k, _)| k).map(|k| k.y).max().unwrap() + 1
+        }
+
+        pub fn get(&self, x: usize, y: usize) -> &NodeMeta {
+            &self.0[&NodePlacement::new(x, y)]
+        }
+    }
+
+    impl FromIterator<String> for Nodes {
+        fn from_iter<I: IntoIterator<Item = String>>(iter: I) -> Self {
+            let pattern =
+                Regex::new(r"^/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+\d+%$")
+                    .unwrap();
+            let values = iter
+                .into_iter()
+                .map(|s| {
+                    let caps = pattern.captures(s.as_str()).unwrap();
+                    let x = caps[1].parse::<usize>().unwrap();
+                    let y = caps[2].parse::<usize>().unwrap();
+                    let u = caps[4].parse::<usize>().unwrap();
+                    let a = caps[5].parse::<usize>().unwrap();
+                    let node_placement = NodePlacement::new(x, y);
+                    let node_meta = NodeMeta {
+                        used: u,
+                        available: a,
+                    };
+                    (node_placement, node_meta)
+                })
+                .collect();
+            Nodes(values)
+        }
     }
 }
 
@@ -45,13 +81,23 @@ pub fn run() {
         .lines()
         .skip(2)
         .filter_map(|l| l.ok())
-        .filter_map(|l| l.parse::<Node>().ok())
-        .collect::<Vec<_>>();
+        .collect::<Nodes>();
 
     let mut result = 0;
-    for i in 0..nodes.len() {
-        for j in 0..nodes.len() {
-            if i != j && nodes[i].used != 0 && nodes[i].used <= nodes[j].available {
+    let width = nodes.width();
+    let height = nodes.height();
+    let size = width * height;
+    for i in 0..size {
+        for j in 0..size {
+            let x_i = i % width;
+            let y_i = i / width;
+            let x_j = j % width;
+            let y_j = j / width;
+            println!("{} - {}", x_i, y_i);
+            if i != j
+                && nodes.get(x_i, y_i).used() != 0
+                && nodes.get(x_i, y_i).used() <= nodes.get(x_j, y_j).available()
+            {
                 result += 1;
             }
         }
